85e958fe62a922cdabcfc78863c39528f3f54f07
tobi00604 <>
2021-02-20 13:42:37 +0900

    abc用のテンプレを試しに作成

---
+++ src/abc000/Main.java
@@ -0,0 +1,23 @@
+package abc000;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// “ü—Í
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.next());
+		long m = Long.parseLong(sc.next());
+		String s = sc.next();
+
+		int[] a = new int[n];
+		long[] b = new long[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = Integer.parseInt(sc.next());
+			b[i] = Long.parseLong(sc.next());
+		}
+
+	}
+}
